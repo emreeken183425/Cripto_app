509c74eff9975461aea49b68179943c776fadf8b
emreeken183425 <emreeken1834@gmail.com>
2022-09-18 15:21:33 +0300

    X

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["criyptoapp.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
